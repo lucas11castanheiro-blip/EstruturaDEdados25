0e8ec279270d7169756c5974d56f5dfc57ae6aff
Lucas Castanheiro <lucas11castanheiro@gmail.com>
2025-08-20 22:06:07 -0300

    Aula3

---
+++ Atividade - Recursividade 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using system;
+namespace RecursionDemo
+{
+
+    class Program
+    {
+        static void main(string[] args)
+        {
+            int x = 3;
+            fun2(x);
+            Console.ReadKey();
+        }
+        static void fun2(int n)
+        {
+            if (n > 0)
+            {
+                fun2(n - 1);
+                Console.Write($"{n} ");
+            }
+        }
+    }
+    
+}
---
+++ Atividade - Recursividade 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace RecursionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int Result = fun(number);
+            Console.WriteLine(Result);
+            Console.ReadKey();
+        }
+        static int fun(int n)
+        {
+            int Result = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                Result = Result + i;
+            }
+            return Result;
+        }
+    }
+}
